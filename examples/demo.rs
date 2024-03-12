0680d2e0404a743494ac04368007a2e698eeccad
Your Name <you@example.com>
2024-03-13 00:29:08 +0100

    Testing egui to make mobile ui

--- examples/demo.rs
+++ examples/demo.rs
@@ -1,10 +1,32 @@
 use {egui_miniquad as egui_mq, miniquad as mq};
 
+struct Content {
+    scroll_to_row_slider: usize,
+    scroll_to_row: Option<usize>,
+    selection: std::collections::HashSet<usize>,
+}
+
 struct Stage {
     egui_mq: egui_mq::EguiMq,
     prev_egui_zoom_factor: f32,
     zoom_factor: f32,
     mq_ctx: Box<dyn mq::RenderingBackend>,
+    content: Content,
+}
+
+fn expanding_content(ui: &mut egui::Ui) {
+    let width = ui.available_width().clamp(20.0, 200.0);
+    let height = ui.available_height();
+    let (rect, _response) = ui.allocate_exact_size(egui::vec2(width, height), egui::Sense::hover());
+    ui.painter().hline(
+        rect.x_range(),
+        rect.center().y,
+        (1.0, ui.visuals().text_color()),
+    );
+}
+
+fn long_text(row_index: usize) -> String {
+    format!("Row {row_index} has some long text that you may want to clip, or it will take up too much horizontal space!")
 }
 
 impl Stage {
@@ -21,43 +43,94 @@ impl Stage {
             prev_egui_zoom_factor: 1.0,
             zoom_factor: 1.0,
             mq_ctx,
+            content: Content {
+                scroll_to_row_slider: 0,
+                scroll_to_row: None,
+                selection: Default::default(),
+            },
+        }
+    }
+}
+
+impl Content {
+    fn table_ui(&mut self, ui: &mut egui::Ui) {
+        use egui_extras::{Column, TableBuilder};
+
+        let text_height = (egui::TextStyle::Button.resolve(ui.style()).size + 32.0)
+            .max(ui.spacing().interact_size.y);
+
+        let mut table = TableBuilder::new(ui)
+            .cell_layout(egui::Layout::left_to_right(egui::Align::Center))
+            .column(Column::remainder())
+            .min_scrolled_height(0.0)
+            .auto_shrink(false);
+
+        // table = table.sense(egui::Sense::click());
+
+        // if let Some(row_index) = self.scroll_to_row.take() {
+        //     table = table.scroll_to_row(row_index, None);
+        // }
+
+        table
+            .header(20.0, |mut header| {
+                header.col(|ui| {
+                    ui.strong("Content");
+                });
+            })
+            .body(|body| {
+                body.rows(text_height, 200, |mut row| {
+                    let row_index = row.index();
+                    // row.set_selected(self.selection.contains(&row_index));
+
+                    row.col(|ui| {
+                        ui.horizontal_centered(|ui| {
+                            ui.add(
+                                egui::Label::new("Thousands of rows of even height").wrap(false),
+                            );
+                            if ui.button("Quit").clicked() {}
+                        });
+                    });
+
+                    // self.toggle_row_selection(row_index, &row.response());
+                });
+            });
+    }
+
+    fn toggle_row_selection(&mut self, row_index: usize, row_response: &egui::Response) {
+        if row_response.clicked() {
+            if self.selection.contains(&row_index) {
+                self.selection.remove(&row_index);
+            } else {
+                self.selection.insert(row_index);
+            }
         }
     }
 
-    fn content(ui: &mut egui::Ui, egui_ctx: &egui::Context, dpi_scale: f32, zoom_factor: &mut f32) {
-        egui::widgets::global_dark_light_mode_buttons(ui);
+    fn content(&mut self, ui: &mut egui::Ui, dpi_scale: f32, zoom_factor: &mut f32) {
+        // egui::widgets::global_dark_light_mode_buttons(ui);
 
-        ui.group(|ui| {
-            ui.label("Physical pixels per each logical 'point':");
-            ui.label(format!("native: {:.2}", dpi_scale));
-            ui.label(format!("egui:   {:.2}", ui.ctx().pixels_per_point()));
-            ui.label("Current zoom factor:");
-            ui.add(egui::Slider::new(zoom_factor, 0.75..=3.0).logarithmic(true))
-                .on_hover_text(
-                    "Override egui zoom factor manually (changes effective pixels per point)",
-                );
-            if ui.button("Reset").clicked() {
-                *zoom_factor = 1.0;
-            }
+        // ui.group(|ui| {
+        //     ui.label("Physical pixels per each logical 'point':");
+        //     ui.label(format!("native: {:.2}", dpi_scale));
+        //     ui.label(format!("egui:   {:.2}", ui.ctx().pixels_per_point()));
+        //     ui.label("Current zoom factor:");
+        //     ui.add(egui::Slider::new(zoom_factor, 0.75..=3.0).logarithmic(true))
+        //         .on_hover_text(
+        //             "Override egui zoom factor manually (changes effective pixels per point)",
+        //         );
+        //     if ui.button("Reset").clicked() {
+        //         *zoom_factor = 1.0;
+        //     }
+        // });
 
-            ui.label("By default, egui allows zooming with\nCtrl/Cmd and +/-/0");
-            // Creating a checkbox that directly mutates the egui context's options causes a
-            // freeze so we copy the state out, possibly mutate it with the checkbox, and
-            // then copy it back in.
-            let mut zoom_with_keyboard = egui_ctx.options(|o| o.zoom_with_keyboard);
-            ui.checkbox(&mut zoom_with_keyboard, "Allow egui zoom with keyboard");
-            egui_ctx.options_mut(|o| o.zoom_with_keyboard = zoom_with_keyboard);
-        });
+        // #[cfg(not(target_arch = "wasm32"))]
+        // {
+        //     if ui.button("Quit").clicked() {
+        //         std::process::exit(0);
+        //     }
+        // }
 
-        #[cfg(not(target_arch = "wasm32"))]
-        {
-            if ui.button("Quit").clicked() {
-                std::process::exit(0);
-            }
-        }
-        if ui.button("bottom button").clicked() {
-            println!("BOTTOM BUTTON!");
-        }
+        self.table_ui(ui);
     }
 }
 
@@ -128,8 +201,12 @@ impl mq::EventHandler for Stage {
                     egui::TextStyle::Button,
                     egui::FontId::new(24.0, egui::epaint::FontFamily::Proportional),
                 );
+                ui.style_mut().text_styles.insert(
+                    egui::TextStyle::Body,
+                    egui::FontId::new(24.0, egui::epaint::FontFamily::Proportional),
+                );
                 ui.vertical_centered_justified(|ui| {
-                    Stage::content(ui, egui_ctx, dpi_scale, zoom_factor);
+                    self.content.content(ui, dpi_scale, zoom_factor);
                 });
             });
 
@@ -186,7 +263,6 @@ impl mq::EventHandler for Stage {
     fn resize_event(&mut self, _width: f32, _height: f32) {
         self.egui_mq.egui_ctx().request_repaint();
     }
-
 }
 
 fn main() {
