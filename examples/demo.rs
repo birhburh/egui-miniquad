1ccde313193b8078db3258a6783bf6e484e6d2a9
Birh Burh <birhburh@gmail.com>
2024-03-18 02:43:13 +0100

    Trying making interface in egui

--- examples/demo.rs
+++ examples/demo.rs
@@ -1,32 +1,12 @@
 use {egui_miniquad as egui_mq, miniquad as mq};
 
-struct Content {
-    scroll_to_row_slider: usize,
-    scroll_to_row: Option<usize>,
-    selection: std::collections::HashSet<usize>,
-}
+struct Content {}
 
 struct Stage {
     egui_mq: egui_mq::EguiMq,
-    prev_egui_zoom_factor: f32,
-    zoom_factor: f32,
     mq_ctx: Box<dyn mq::RenderingBackend>,
     content: Content,
-}
-
-fn expanding_content(ui: &mut egui::Ui) {
-    let width = ui.available_width().clamp(20.0, 200.0);
-    let height = ui.available_height();
-    let (rect, _response) = ui.allocate_exact_size(egui::vec2(width, height), egui::Sense::hover());
-    ui.painter().hline(
-        rect.x_range(),
-        rect.center().y,
-        (1.0, ui.visuals().text_color()),
-    );
-}
-
-fn long_text(row_index: usize) -> String {
-    format!("Row {row_index} has some long text that you may want to clip, or it will take up too much horizontal space!")
+    first: bool,
 }
 
 impl Stage {
@@ -35,102 +15,72 @@ impl Stage {
 
         let egui_mq = egui_mq::EguiMq::new(&mut *mq_ctx);
 
-        #[cfg(target_os = "macos")]
-        load_system_font(egui_mq.egui_ctx());
+        // #[cfg(target_os = "macos")]
+        // load_system_font(egui_mq.egui_ctx());
 
         Self {
             egui_mq,
-            prev_egui_zoom_factor: 1.0,
-            zoom_factor: 1.0,
             mq_ctx,
-            content: Content {
-                scroll_to_row_slider: 0,
-                scroll_to_row: None,
-                selection: Default::default(),
-            },
+            content: Content {},
+            first: true,
         }
     }
 }
 
 impl Content {
-    fn table_ui(&mut self, ui: &mut egui::Ui) {
-        use egui_extras::{Column, TableBuilder};
+    fn content(&mut self, ui: &mut egui::Ui) {
+        egui::Frame::canvas(ui.style()).show(ui, |ui| {
+            let desired_width = ui.available_width();
+            let desired_height = ui.available_height();
+            let desired_size = egui::vec2(desired_width, desired_height);
+            let (_id, rect) = ui.allocate_space(desired_size);
 
-        let text_height = (egui::TextStyle::Button.resolve(ui.style()).size + 32.0)
-            .max(ui.spacing().interact_size.y);
+            let painter = ui.painter();
+            painter.rect(
+                rect.shrink(1.0),
+                10.0,
+                ui.ctx().style().visuals.window_fill(),
+                egui::Stroke::new(0.5, egui::Color32::DARK_GRAY),
+            );
+            painter.line_segment(
+                [
+                    rect.left_top() + egui::vec2(2.0, rect.height() * 0.5),
+                    rect.right_top() + egui::vec2(-2.0, rect.height() * 0.5),
+                ],
+                egui::Stroke::new(0.5, egui::Color32::DARK_GREEN),
+            );
+            let pos_top_left =
+                egui::emath::pos2(rect.left() + (rect.width() / 2.0) - 30.0, rect.top() + 20.0);
+            let pos_bottom_right = egui::emath::pos2(
+                rect.left() + (rect.width() / 2.0) + 30.0,
+                rect.top() + rect.height() - 20.0,
+            );
+            let r = egui::emath::Rect::from_two_pos(pos_top_left, pos_bottom_right);
 
-        let mut table = TableBuilder::new(ui)
-            .cell_layout(egui::Layout::left_to_right(egui::Align::Center))
-            .column(Column::remainder())
-            .min_scrolled_height(0.0)
-            .auto_shrink(false);
-
-        // table = table.sense(egui::Sense::click());
-
-        // if let Some(row_index) = self.scroll_to_row.take() {
-        //     table = table.scroll_to_row(row_index, None);
-        // }
-
-        table
-            .header(20.0, |mut header| {
-                header.col(|ui| {
-                    ui.strong("Content");
-                });
-            })
-            .body(|body| {
-                body.rows(text_height, 200, |mut row| {
-                    let row_index = row.index();
-                    // row.set_selected(self.selection.contains(&row_index));
-
-                    row.col(|ui| {
-                        ui.horizontal_centered(|ui| {
-                            ui.add(
-                                egui::Label::new("Thousands of rows of even height").wrap(false),
-                            );
-                            if ui.button("Quit").clicked() {}
-                        });
-                    });
-
-                    // self.toggle_row_selection(row_index, &row.response());
-                });
-            });
+            painter.rect_filled(r, 10.0, egui::ecolor::Rgba::from_luminance_alpha(0.2, 0.2));
+            painter.text(
+                egui::pos2(
+                    rect.left() + rect.width() * 0.2,
+                    rect.top() + rect.height() * 0.7,
+                ),
+                egui::Align2::LEFT_CENTER,
+                "This is some text",
+                egui::FontId::new(30.0, egui::FontFamily::Proportional),
+                egui::Color32::RED,
+            );
+        });
     }
 
-    fn toggle_row_selection(&mut self, row_index: usize, row_response: &egui::Response) {
-        if row_response.clicked() {
-            if self.selection.contains(&row_index) {
-                self.selection.remove(&row_index);
-            } else {
-                self.selection.insert(row_index);
-            }
-        }
-    }
-
-    fn content(&mut self, ui: &mut egui::Ui, dpi_scale: f32, zoom_factor: &mut f32) {
-        // egui::widgets::global_dark_light_mode_buttons(ui);
-
-        // ui.group(|ui| {
-        //     ui.label("Physical pixels per each logical 'point':");
-        //     ui.label(format!("native: {:.2}", dpi_scale));
-        //     ui.label(format!("egui:   {:.2}", ui.ctx().pixels_per_point()));
-        //     ui.label("Current zoom factor:");
-        //     ui.add(egui::Slider::new(zoom_factor, 0.75..=3.0).logarithmic(true))
-        //         .on_hover_text(
-        //             "Override egui zoom factor manually (changes effective pixels per point)",
-        //         );
-        //     if ui.button("Reset").clicked() {
-        //         *zoom_factor = 1.0;
-        //     }
-        // });
-
-        // #[cfg(not(target_arch = "wasm32"))]
-        // {
-        //     if ui.button("Quit").clicked() {
-        //         std::process::exit(0);
-        //     }
-        // }
-
-        self.table_ui(ui);
+    fn expanding_content(ui: &mut egui::Ui) {
+        let width = ui.available_width().clamp(20.0, 200.0);
+        let height = ui.available_height();
+        let (rect, _response) =
+            ui.allocate_exact_size(egui::vec2(width, height), egui::Sense::hover());
+        ui.painter().hline(
+            rect.x_range(),
+            rect.center().y,
+            (1.0, ui.visuals().text_color()),
+        );
     }
 }
 
@@ -178,43 +128,68 @@ fn load_system_font(ctx: &egui::Context) {
 }
 
 impl mq::EventHandler for Stage {
-    fn update(&mut self) -> bool {
-        let dpi_scale = mq::window::dpi_scale();
-
-        let zoom_factor = &mut self.zoom_factor;
+    fn update(&mut self, skiped: bool) -> bool {
         // Run the UI code:
-        self.egui_mq.run(&mut *self.mq_ctx, |_mq_ctx, egui_ctx| {
-            // zoom factor could have been changed by the user in egui using Ctrl/Cmd and -/+/0,
-            // but it could also be in the middle of being changed by us using the slider. So we
-            // only allow egui's zoom to override our zoom if the egui zoom is different from what
-            // we saw last time (meaning the user has changed it).
-            let curr_egui_zoom = egui_ctx.zoom_factor();
-            if self.prev_egui_zoom_factor != curr_egui_zoom {
-                *zoom_factor = curr_egui_zoom;
-            }
-            self.prev_egui_zoom_factor = curr_egui_zoom;
+        self.egui_mq
+            .run(&mut *self.mq_ctx, skiped, |_mq_ctx, egui_ctx| {
+                #[cfg(target_os = "macos")]
+                if self.first {
+                    self.first = false;
+                    egui_ctx.request_repaint();
+                }
 
-            egui::CentralPanel::default().show(egui_ctx, |ui| {
-                ui.style_mut().spacing.button_padding = egui::vec2(16.0, 16.0);
-                // ui.style_mut().spacing.slider_width = 200.0;
-                ui.style_mut().text_styles.insert(
-                    egui::TextStyle::Button,
-                    egui::FontId::new(24.0, egui::epaint::FontFamily::Proportional),
-                );
-                ui.style_mut().text_styles.insert(
-                    egui::TextStyle::Body,
-                    egui::FontId::new(24.0, egui::epaint::FontFamily::Proportional),
-                );
-                ui.vertical_centered_justified(|ui| {
-                    self.content.content(ui, dpi_scale, zoom_factor);
+                egui::TopBottomPanel::bottom("tools").show(egui_ctx, |ui| {
+                    ui.style_mut().spacing.button_padding = egui::vec2(16.0, 16.0);
+                    // ui.style_mut().spacing.slider_width = 200.0;
+                    ui.style_mut().text_styles.insert(
+                        egui::TextStyle::Button,
+                        egui::FontId::new(24.0, egui::epaint::FontFamily::Proportional),
+                    );
+                    ui.style_mut().text_styles.insert(
+                        egui::TextStyle::Body,
+                        egui::FontId::new(24.0, egui::epaint::FontFamily::Proportional),
+                    );
+
+                    let scroll_bar_visibility = if ! cfg!(target_os = "android") {
+                        egui::scroll_area::ScrollBarVisibility::VisibleWhenNeeded
+                    } else {
+                        egui::scroll_area::ScrollBarVisibility::AlwaysHidden
+                    };
+                    egui::ScrollArea::horizontal()
+                        .drag_to_scroll(true)
+                        .scroll_bar_visibility(scroll_bar_visibility)
+                        .show(ui, |ui| {
+                            ui.with_layout(egui::Layout::left_to_right(egui::Align::Max), |ui| {
+                                if ui.button("🔍").clicked() {
+                                    mq::window::set_window_size(100, 100);
+                                }
+                                if ui.button("🛃").clicked() {
+                                    mq::window::set_window_size(300, 300);
+                                }
+                                if ui.button("💛").clicked() {
+                                    mq::window::set_window_size(500, 500);
+                                }
+                                if ui.button("💚").clicked() {
+                                    mq::window::set_window_size(700, 700);
+                                }
+                            });
+                        });
+                });
+
+                egui::CentralPanel::default().show(egui_ctx, |ui| {
+                    ui.style_mut().spacing.button_padding = egui::vec2(16.0, 16.0);
+                    // ui.style_mut().spacing.slider_width = 200.0;
+                    ui.style_mut().text_styles.insert(
+                        egui::TextStyle::Button,
+                        egui::FontId::new(24.0, egui::epaint::FontFamily::Proportional),
+                    );
+                    ui.style_mut().text_styles.insert(
+                        egui::TextStyle::Body,
+                        egui::FontId::new(24.0, egui::epaint::FontFamily::Proportional),
+                    );
+                    self.content.content(ui);
                 });
             });
-
-            // Don't change zoom while dragging the slider
-            if !egui_ctx.is_using_pointer() {
-                egui_ctx.set_zoom_factor(*zoom_factor);
-            }
-        });
         self.egui_mq.egui_ctx().has_requested_repaint()
     }
 
@@ -271,6 +246,7 @@ fn main() {
         high_dpi: true,
         window_width: 500,
         window_height: 300,
+        min_width: Some(300),
         ..Default::default()
     };
     mq::start(conf, || Box::new(Stage::new()));
